e30d6cb55b7d7db4bdbdf6254fe7ae099ceba92c
Ymerejx <loaw145@gmail.com>
2024-08-05 23:47:56 +0200

    phpmyadmin

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ let pool;
 let conn;
 async function handleDisconnect() {
   pool = mariadb.createPool({
-    host: '192.168.1.32',  // Remplacez par l'adresse de votre serveur MariaDB
-    user: 'root',  // Remplacez par votre nom d'utilisateur
+    host: 'localhost',  // Remplacez par l'adresse de votre serveur MariaDB
+    user: 'phpmyadmin',  // Remplacez par votre nom d'utilisateur
     password: '149999',  // Remplacez par votre mot de passe
     connectionLimit: 5
   });
